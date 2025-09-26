ac7bf75c41dd7f26f82434e4ffca1252cb780621
xuannhiminh <xuannhiminh>
2025-09-26 10:42:16 +0700

    update new key

--- app/src/main/java/xlsx/reader/spread/sheets/viewer/editor/utils/AppUtils.kt
+++ app/src/main/java/xlsx/reader/spread/sheets/viewer/editor/utils/AppUtils.kt
@@ -20,6 +20,6 @@ class AppUtils {
         }
          const val PDF_DETAIL_EZLIB = 0L // 0 mean use EZ lib pdf detail, 1 mean use SoLib PDF detail
          const val FOLDER_EXTERNAL_IN_DOWNLOADS = "SheetsReaderGB"
-         const val PUBLIC_LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAh9aRvWOCOxbhgeK3Yxn5AwxFuU1UsbCs9AJ1ksGKcmFPDpIpV37doDoH9Ti8Bvw08Tmpm2X5ZJ0/mufA1UJYb7bYLjmqLMWyw9NLdp+ZVeCFqbCLeEbuVtx+xh00yNjYDdtWeYUEVb8JD3CtLQ8Lt4yGO9wyeQaadIvjPLoqLZujM8cfrFm9hhFwpkPIj8w6pXfJUC1ZQ+xP+sZ8Ul2MZq/k6qzAbmsMLukT/tJYHjfCBqxndpskNpDdnz5IlzuofIo8qpCO2d+LSCjlq4xeIBaXDulcilufsEYN0K22tijj0BKRs0pxuazsvMUNmft2t4kw2WwFdTa9JmiH1KYjNQIDAQAB"
+         const val PUBLIC_LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxF8vXD0+tyiwa63y0efkpf0WvwzZh2ZkqXE6h4y8XJt5YoAq9Mf96qMvZcHvi18sv5bxZLGSIJOA8+k1ixM8gK38BkuKQN2p2sx9t3VvGdesKqNEHEdQXvYjPgg2GxMv2fSvW3NKoeN1r9ueUr7xcf6WZFtCsZCEyWr//U7nSzfGFQZBz3muLoOAjPaiUUs7j4xrW/2KbUKeplBRrJDC7BPMLivhH4B+bGO2WO+9YtCU0DGt8OY9AgYQQEqw4RDG72Mxxi9/PqzSmz7utTx+R83n3f6o54PbmbH/QNXj3upNAwbM2apLVSg6i7YTZbfvAR+2JzP31BwpI9MOmD2WFQIDAQAB"
      }
  }
